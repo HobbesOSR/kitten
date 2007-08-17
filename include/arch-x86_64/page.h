b13ebc535403d9bfd44b824833fa5f65d71aafd3
Kevin Pedretti <ktpedre@sandia.gov>
2007-08-17 11:55:59 -0600

    Initial version

---
+++ include/arch-x86_64/page.h
@@ -0,0 +1,28 @@
+#ifndef _X86_64_PAGE_H
+#define _X86_64_PAGE_H
+
+#define PAGE_SHIFT		12
+
+/**
+ * CAREFUL... Each of the following defines has two versions, one for
+ *            assembly and another for C.  Be sure to update both!
+ */
+#ifndef __ASSEMBLY__
+#define PAGE_SIZE		(1UL << PAGE_SHIFT)
+#define __PHYSICAL_START        ((unsigned long)CONFIG_PHYSICAL_START)
+#define __START_KERNEL          (__START_KERNEL_map + __PHYSICAL_START)
+#define __START_KERNEL_map      0xffffffff80000000UL
+#define __PAGE_OFFSET           0xffff810000000000UL
+#else
+#define PAGE_SIZE		(0x1 << PAGE_SHIFT)
+#define __PHYSICAL_START        CONFIG_PHYSICAL_START
+#define __START_KERNEL          (__START_KERNEL_map + __PHYSICAL_START)
+#define __START_KERNEL_map      0xffffffff80000000
+#define __PAGE_OFFSET           0xffff810000000000
+#endif
+
+#define THREAD_ORDER 1 
+#define THREAD_SIZE  (PAGE_SIZE << THREAD_ORDER)
+#define CURRENT_MASK (~(THREAD_SIZE-1))
+
+#endif
