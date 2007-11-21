838eeba9edd037c21344129387b0898aaabd50e3
Kevin Pedretti <ktpedre@sandia.gov>
2007-11-21 10:31:35 -0700

    Change name of l0 console to match Cray's name... rcal0.

--- drivers/console/rcal0.c
+++ drivers/console/rcal0.c
@@ -23,21 +23,21 @@ static void l0_write(struct console *con, const char *str)
 			ch_send_data(L0RCA_CH_CON_UP, &ev_hdr, (void *)str, n))
 		     <= 0)
 		{
-			// either error or we are done
+			/* either error or we are done */
 			break;
 		}
 
 		if (n > ret) {
-			// some bytes were sent, point to the remaining data
+			/* some bytes were sent, point to the remaining data */
 			str += (n - ret);
 			n = ret;
 		}
 
-		// busy wait if the buf is full
+		/* busy wait if the buf is full */
                 udelay(1000);
 	}
 
-	// if error, give up and spin forever
+	/* if error, give up and spin forever */
 	if (ret < 0)
 		while (1) {}
 
@@ -48,7 +48,7 @@ static void l0_write(struct console *con, const char *str)
  * Cray L0 console device.
  */
 static struct console l0_console = {
-	.name  = "Cray L0 Console",
+	.name  = "Cray RCA L0 Console",
 	.write = l0_write
 };
 
@@ -62,10 +62,10 @@ void l0_console_init(void)
 		return;
 	}
 
-	// Read the configuration information provided by the L0
+	/* Read the configuration information provided by the L0 */
 	l0rca_init_config();
 
-	// Setup the event template to use for outgoing events
+	/* Setup the event template to use for outgoing events */
 	ev_hdr.ev_id		=	ec_console_log;
 	ev_hdr.ev_gen		=	RCA_MKSVC(
 						RCA_INST_ANY,
@@ -75,12 +75,12 @@ void l0_console_init(void)
 	ev_hdr.ev_src		=	ev_hdr.ev_gen;
 	ev_hdr.ev_priority	=	RCA_LOG_DEBUG;
 	ev_hdr.ev_flag		=	0;
-	// Timestamp, len & data is filled at the time of sending event
+	/* Timestamp, len & data is filled at the time of sending event */
 	
-	// Register with the Cray RCA subsystem
+	/* Register with the Cray RCA subsystem */
 	register_ch_up(L0RCA_CH_CON_UP, NULL, 0, -1);
 
-	// Register the L0 console with the LWK
+	/* Register the L0 console with the LWK */
 	console_register(&l0_console);
 	initialized = 1;
 }
