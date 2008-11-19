70790d74d5be2ac0748d437e1aa8c6acaf5198ee
Trammell Hudson <hudson@osresearch.net>
2008-11-19 14:21:53 -0700

    Test sockets api from user level

--- user/hello_world/hello_world.c
+++ user/hello_world/hello_world.c
@@ -5,9 +5,13 @@
 #include <unistd.h>
 #include <limits.h>
 #include <lwk/liblwk.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
 static void pmem_api_test(void);
 static void aspace_api_test(void);
+static void socket_api_test(void);
 
 int
 main(int argc, char *argv[], char *envp[])
@@ -27,10 +31,102 @@ main(int argc, char *argv[], char *envp[])
 
 	pmem_api_test();
 	aspace_api_test();
+	socket_api_test();
 
 	printf("Spinning forever...\n");
 	while (1) {}
 }
+
+
+void
+socket_api_test( void )
+{
+	printf( "Testing socket\n" );
+	int port = 80;
+	int s = socket( PF_INET, SOCK_STREAM, 0 );
+	if( s < 0 )
+	{
+		printf( "socket failed! rc=%d\n", s );
+		return;
+	}
+
+	struct sockaddr_in addr, client;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons( port );
+	addr.sin_addr.s_addr = INADDR_ANY;
+	if( bind( s, (struct sockaddr*) &addr, sizeof(addr) ) < 0 )
+		perror( "bind" );
+
+	if( listen( s, 5 ) < 0 )
+		perror( "listen" );
+
+	int max_fd = s;
+	fd_set fds;
+	FD_ZERO( &fds );
+	FD_SET( s, &fds );
+
+	printf( "Going into mainloop: server fd %d on port %d\n", s, port );
+
+	while(1)
+	{
+		fd_set read_fds = fds;
+		int rc = select( max_fd+1, &read_fds, 0, 0, 0 );
+		if( rc < 0 )
+		{
+			printf( "select failed! rc=%d\n", rc );
+			break;
+		}
+
+		if( FD_ISSET( s, &read_fds ) )
+		{
+			printf( "new connection\n" );
+			int socklen = sizeof(client);
+			int new_fd = accept(
+				s,
+				(struct sockaddr *) &client,
+				&socklen
+			);
+
+			printf( "connected newfd %d!\n", new_fd );
+			FD_SET( new_fd, &fds );
+			if( new_fd > max_fd )
+				max_fd = new_fd;
+		}
+
+		int fd;
+		for( fd=0 ; fd<=max_fd ; fd++ )
+		{
+			if( fd == s || !FD_ISSET( fd, &read_fds ) )
+				continue;
+
+			char buf[ 33 ];
+			ssize_t rc = read( fd, buf, sizeof(buf)-1 );
+			if( rc <= 0 )
+			{
+				printf( "%d: closed connection\n", fd );
+				FD_CLR( fd, &fds );
+				continue;
+			}
+
+			buf[rc] = '\0';
+
+			// Trim any newlines off the end
+			while( rc > 0 )
+			{
+				if( !isspace( buf[rc-1] ) )
+					break;
+				buf[--rc] = '\0';
+			}
+
+			printf( "%d: read %d bytes: '%s'\n",
+				fd,
+				rc,
+				buf
+			);
+		}
+	}
+}
+
 
 static void
 pmem_api_test(void)
