8d9759b613227b9bd9c9a390e314a24014194a5d
Jack Lange <jarusl@cs.northwestern.edu>
2009-02-10 09:56:39 -0700

    Special console device for use by Palacios guests.

---
+++ drivers/console/vm.c
@@ -0,0 +1,81 @@
+/* Console Driver when Kitten is running as a Palacios guest
+ * Jack Lange (jarusl@cs.northwestern.edu)
+ */
+
+#include <lwk/delay.h>
+#include <lwk/driver.h>
+#include <lwk/console.h>
+#include <arch/io.h>
+#include <lwk/interrupt.h>
+
+
+/**
+ * IO port address of the vm console.
+ *
+ * \note This should be set to an unused port.
+ */
+static unsigned int port = 0xc0c0;
+
+
+/** Set when the vm console has been initialized. */
+static int initialized = 0;
+
+
+/**
+ * Prints a single character to the vm console port.
+ */
+static void vm_cons_putc(struct console *con, unsigned char c)
+{
+	// Slam the 8 bits down the 1 bit pipe... meeeooowwwy!
+	outb(c, port);
+}
+
+
+/**
+ * Writes a string to the vm console port.
+ */
+static void vm_cons_write(struct console *con, const char *str)
+{
+	unsigned char c;
+	while ((c = *str++) != '\0') {
+		vm_cons_putc(con, c);
+	}
+}
+
+
+/**
+ * Reads a single character from the vm console port.
+ */
+static char vm_cons_getc(struct console *con)
+{
+	return inb_p(port);
+}
+
+
+/**
+ * Console device for use by guest virtual machines.
+ */
+static struct console vm_console = {
+	.name		= "Virtual Machine Console",
+	.write		= vm_cons_write,
+	.poll_get_char	= vm_cons_getc,
+	.poll_put_char	= vm_cons_putc
+};
+
+
+/**
+ * Initializes and registers the vm console driver.
+ */
+void vm_console_init(void) {
+	if (initialized) {
+		printk(KERN_ERR "VM console already initialized.\n");
+		return;
+	}
+
+	console_register(&vm_console);
+	initialized = 1;
+}
+
+
+driver_init("console", vm_console_init);
+driver_param(port, uint);
