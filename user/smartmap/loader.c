5fc2a0128814a65ec1ff98c5e213ecf3ce74a14d
Kevin Pedretti <ktpedre@sandia.gov>
2009-06-16 16:11:25 -0600

    Add SMARTMAP test program to user/ directory. The resulting 'loader' app can be used in place of the normal hello_world init_task.  The loader app launches the ELF app embedded in its image, 'smartmap_app', and sets up the SMARTMAP mappings. One instance of smartmap_app is started on each core.

---
+++ user/smartmap/loader.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <lwk/liblwk.h>
+
+
+/* The linker sets this up to point to the embedded smartmap_app ELF image */
+int _binary_loader_rawdata_start __attribute__ ((weak));
+
+
+int
+main(int argc, char *argv[], char *envp[])
+{
+	volatile vaddr_t elf_image = (vaddr_t) &_binary_loader_rawdata_start;
+	start_state_t *start_state;
+	user_cpumask_t cpumask;
+	int num_ranks=0;
+	int status;
+	int cpu, rank;
+	int src, dst;
+
+	/* Figure out how many CPUs are available */
+	printf("Available cpus:\n    ");
+	task_get_cpumask(&cpumask);
+	for (cpu = CPU_MIN_ID; cpu <= CPU_MAX_ID; cpu++) {
+		if (cpu_isset(cpu, cpumask)) {
+			printf("%d ", cpu);
+			++num_ranks;
+		}
+	}
+	printf("\n");
+
+	/* Allocate a start_state structure for each rank */
+	start_state = malloc(num_ranks * sizeof(start_state_t));
+	if (!start_state) {
+		printf("malloc of start_state[] failed\n");
+		exit(-1);
+	}
+
+	/* Create an address space for each rank, one per CPU */
+	printf("Creating address spaces...\n");
+	for (cpu = CPU_MIN_ID, rank = 0; cpu <= CPU_MAX_ID; cpu++, rank++) {
+		if (!cpu_isset(cpu, cpumask))
+			continue;
+
+		start_state[rank].cpu_id  = cpu;
+		start_state[rank].uid     = 1; /* anything but 0(=root) */
+		start_state[rank].gid     = 1;
+		start_state[rank].cpumask = NULL;
+
+		status =
+		elf_load(
+			(void *)elf_image,
+			"smartmap_app",
+			ASPACE_MIN_ID + rank,
+			PAGE_SIZE,
+			(1024 * 1024 * 16),  /* heap_size  = 16 MB */
+			(1024 * 256),        /* stack_size = 256 KB */
+			"",                  /* argv_str */
+			"",                  /* envp_str */
+			&start_state[rank],
+			0,
+			&elf_dflt_alloc_pmem
+		);
+
+		if (status) {
+			printf("elf_load failed, status=%d\n", status);
+			exit(-1);
+		}
+	}
+	printf("   OK\n");
+
+	/* Create SMARTMAP mappings for each rank */
+	printf("Creating SMARTMAP mappings...\n");
+	for (dst = 0; dst < num_ranks; dst++) {
+		for (src = 0; src < num_ranks; src++) {
+			if (src == dst)
+				continue;
+
+			status =
+			aspace_smartmap(
+				start_state[src].aspace_id,
+				start_state[dst].aspace_id,
+				SMARTMAP_ALIGN + (SMARTMAP_ALIGN * src),
+				SMARTMAP_ALIGN
+			);
+
+			if (status) {
+				printf("smartmap failed, status=%d\n", status);
+				exit(-1);
+			}
+		}
+	}
+	printf("   OK\n");
+
+	/* Create a task for each rank */
+	printf("Creating tasks...\n");
+	for (rank = 0; rank < num_ranks; rank++) {
+		char task_name[32];
+		sprintf(task_name, "RANK%d", rank);
+
+		status =
+		task_create(
+			TASK_MIN_ID + rank,
+			task_name,
+			&start_state[rank],
+			NULL
+		);
+
+		if (status) {
+			printf("task_create failed, status=%d\n", status);
+			exit(-1);
+		}
+	}
+	printf("   OK\n");
+
+	printf("LOADER DONE.\n");
+
+	/* Go to sleep "forever" */
+	while (1)
+		sleep(100000);
+}
---
+++ user/smartmap/smartmap_app.c
@@ -0,0 +1,85 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sched.h>
+
+
+#define DATA_COUNT 10
+int data[DATA_COUNT];
+
+
+/**
+ * Magic function for converting local address to (intra-node) remote address.
+ */
+static inline void *
+remote_address(
+        unsigned	rank,
+        void *		vaddr
+)
+{
+	uintptr_t addr = (uintptr_t) vaddr;
+	addr |= ((uintptr_t) (rank+1)) << 39;
+
+	return (void*) addr;
+}
+
+
+/**
+ * Returns the local task's rank.
+ * This assumes the low 8-bits of the task ID are the rank.
+ */
+int
+get_my_rank(void)
+{
+	pid_t pid = getpid();
+	return (pid & 0xFF);
+}
+
+
+/**
+ * Returns the number of ranks on the node, numbered [0,num_ranks).
+ */
+int
+get_num_ranks(void)
+{
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	sched_getaffinity(0, sizeof(cpuset), &cpuset);
+	return CPU_COUNT(&cpuset);
+}
+
+
+/**
+ * A very simple SMARTMAP test program.
+ */
+int main( int argc, char **argv )
+{
+	int rank, i;
+	int *datap;
+
+	/* Initialize each rank's data array to contain the rank's ID */
+	for (i=0; i<DATA_COUNT; i++)
+		data[i] = get_my_rank();
+
+	printf("Simulating barrier by sleeping 3 secs\n");
+	sleep(3);
+
+	/* Rank 0 print's out each other rank's data array */
+	if (get_my_rank() == 0) {
+		for (rank = 1; rank < get_num_ranks(); rank++) {
+			datap = (int *)remote_address(rank, &data);
+
+			printf("Data for rank %d:\n", rank);
+			for (i = 0; i < DATA_COUNT; i++) {
+				printf("data[%d] = %d\n", i, datap[i]);
+			}
+		}
+	}
+
+	/* Sleep "forever" */
+	while (1)
+		sleep(100000);
+}
