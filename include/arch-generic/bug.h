99c87603b3175e9a106c96933022e422124b2569
Kevin Pedretti <ktpedre@sandia.gov>
2009-08-13 16:29:52 -0600

    Add WARN_ON() and WARN_ON_ONCE()

--- include/arch-generic/bug.h
+++ include/arch-generic/bug.h
@@ -19,13 +19,35 @@
 #endif
 
 #ifndef HAVE_ARCH_WARN_ON
-#define WARN_ON(condition) do { \
-	if (unlikely((condition)!=0)) { \
+#define WARN_ON(condition) ({					\
+	int __ret_warn_on = !!(condition);			\
+	if (unlikely(__ret_warn_on)) {				\
 		printk("BUG: warning at %s:%d/%s()\n", __FILE__, __LINE__, __FUNCTION__); \
-		/* TODO FIX ME */ \
-		/* dump_stack(); */ \
-	} \
-} while (0)
+		/* TODO FIX ME */				\
+		/* dump_stack(); */				\
+	}							\
+	unlikely(__ret_warn_on);				\
+})
 #endif
+
+#define WARN_ON_ONCE(condition)	({				\
+	static int __warned;					\
+	int __ret_warn_once = !!(condition);			\
+								\
+	if (unlikely(__ret_warn_once))				\
+		if (WARN_ON(!__warned)) 			\
+			__warned = 1;				\
+	unlikely(__ret_warn_once);				\
+})
+
+#define WARN_ONCE(condition, format...)	({			\
+	static int __warned;					\
+	int __ret_warn_once = !!(condition);			\
+								\
+	if (unlikely(__ret_warn_once))				\
+		if (WARN(!__warned, format)) 			\
+			__warned = 1;				\
+	unlikely(__ret_warn_once);				\
+})
 
 #endif
