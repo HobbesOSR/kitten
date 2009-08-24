12a616146005409d7e82c17355d05f5266745609
Kevin Pedretti <ktpedre@sandia.gov>
2009-08-24 12:50:56 -0600

    Changes to Linux Kconfig scheme.

--- linux/include/linux/kallsyms.h
+++ linux/include/linux/kallsyms.h
@@ -29,4 +29,19 @@ print_symbol(const char *fmt, unsigned long address)
 	printk(fmt, buffer);
 }
 
+/*
+ * Pretty-print a function pointer.
+ *
+ * ia64 and ppc64 function pointers are really function descriptors,
+ * which contain a pointer the real address.
+ */
+static inline void
+print_fn_descriptor_symbol(const char *fmt, void *address)
+{
+	#if defined(CONFIG_IA64) || defined(CONFIG_PPC64)
+        address = *(void **)address;
+	#endif
+	print_symbol(fmt, (unsigned long)address);
+}
+
 #endif
