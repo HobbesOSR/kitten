24880ab491721c8152f3d932ce08e28856d53983
Kevin Pedretti <ktpedre@sandia.gov>
2009-08-26 14:40:49 -0600

    Fixups to Linux io_apic.h header

--- linux/include/asm-x86_64/io_apic.h
+++ linux/include/asm-x86_64/io_apic.h
@@ -3,11 +3,17 @@
 
 #include <arch/io_apic.h>
 
-/*
- * LWK always uses the I/O APIC for interrupt routing, so disable
- * automatic assignment of PCI IRQ's.
+/**
+ * LWK always uses the I/O APIC for interrupt routing,
+ * so disable automatic assignment of PCI IRQ's.
  */
 #define io_apic_assign_pci_irqs 1
+
+/**
+ * LWK uses ioapic_num to count the number of IO APICs,
+ * Linux uses nr_ioapics.
+ */
+#define nr_ioapics ioapic_num
 
 static inline int
 IO_APIC_get_PCI_irq_vector(int bus, int slot, int pin)
