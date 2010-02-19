07ce46b0a74a699dcc092ec2deb4fefd376e8122
mjleven <mjleven@gato.sandia.gov>
2010-02-19 16:26:25 -0700

    add some #defines

--- linux/include/linux/notifier.h
+++ linux/include/linux/notifier.h
@@ -9,6 +9,10 @@ struct blocking_notifier_head { };
 
 #define BLOCKING_INIT_NOTIFIER_HEAD(name)
 
+#define NETDEV_BONDING_FAILOVER 0x000C
+
+#define NOTIFY_DONE     0x0000      /* Don't care */
+
 extern int blocking_notifier_call_chain(struct blocking_notifier_head *nh, unsigned long val, void *v);
 extern int blocking_notifier_chain_register(struct blocking_notifier_head *nh, struct notifier_block *nb);
 extern int blocking_notifier_chain_unregister(struct blocking_notifier_head *nh, struct notifier_block *nb);
