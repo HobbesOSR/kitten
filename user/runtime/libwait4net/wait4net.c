898751da692ae7352bd1306047a04fed1f5880ae
mjleven <mjleven@gato.sandia.gov>
2010-04-19 14:51:15 -0600

    Move non-lwk syscall to seperate file This minimizes what is needed in the lwk system call headers.

--- user/runtime/libwait4net/wait4net.c
+++ user/runtime/libwait4net/wait4net.c
@@ -8,7 +8,7 @@ static int ifconfig( __u32 ip_addr, __u32 netmask, union ibv_gid* gid );
 static int wait_active( struct ibv_context* ctx, int num_ports, int seconds,
 				int* port, int* lid );
 
-static __u32 ip_addr; 
+__u32 __ip_addr; 
 static __attribute__ ((constructor (65534))) void init(void)
 {
 	struct ibv_device** list = NULL;
@@ -51,13 +51,13 @@ static __attribute__ ((constructor (65534))) void init(void)
        					__be64_to_cpu(gid.global.subnet_prefix),
         				__be64_to_cpu(gid.global.interface_id));
 
-				ip_addr = 0x0a000000 + lid; 	
+				__ip_addr = 0x0a000000 + lid; 	
 				__u32 netmask = 0xffffff00;
 
 				char buf[16];
 				sprintf(buf,"node-%d",lid);
 				sethostname(buf,strlen(buf));
-				ifconfig( ip_addr, netmask, &gid);
+				ifconfig( __ip_addr, netmask, &gid);
 				ibv_close_device( ctx); 
 				ibv_free_device_list( list );
 				return; 
@@ -103,66 +103,4 @@ static int ifconfig( __u32 ip_addr, __u32 netmask, union ibv_gid* gid )
 	memset( req.ifr_hwaddr, 0, MAX_ADDR_LEN );
 	memcpy( req.ifr_hwaddr + 4, gid->raw, 16 );
 	return lwk_ifconfig(&req);
-}
-
-#include <stdlib.h>
-
-      struct addrinfo {
-           int     ai_flags;
-           int     ai_family;
-           int     ai_socktype;
-           int     ai_protocol;
-           size_t  ai_addrlen;
-           struct sockaddr *ai_addr;
-           char   *ai_canonname;
-           struct addrinfo *ai_next;
-       };
-
-
-int getaddrinfo(const char *node, const char *service,
-                       const struct addrinfo *hints,
-                       struct addrinfo **res)
-{
-	struct addrinfo* resPtr=  malloc( sizeof( *resPtr) );
-	struct sockaddr_in* addr_in = malloc(sizeof(*addr_in));
-
-
-	if ( ! strncmp( node, "ib0", 3 ) ) {
-    		addr_in->sin_addr.s_addr = __cpu_to_be32(ip_addr);
-	} else {
-        	int tmp[4];
-
-        	sscanf( node, "%d.%d.%d.%d", tmp+3, tmp+2, tmp+1, tmp );
-
-        	__be32 addr = ( tmp[3] << 24 ) | ( tmp[2] << 16 ) |
-                                                ( tmp[1] << 8 ) | tmp[0];
-
-    		addr_in->sin_addr.s_addr = __cpu_to_be32(addr);
-	}
-#if 1 
-	printf("%s() node=%s %#x\n",__FUNCTION__,node,
-			addr_in->sin_addr.s_addr);
-#endif
-	
-	addr_in->sin_port = 0;
-
-	resPtr->ai_flags = 40;
-	resPtr->ai_family = AF_INET;
-	resPtr->ai_socktype = SOCK_STREAM;
-	resPtr->ai_protocol = IPPROTO_TCP;
-	resPtr->ai_addrlen = sizeof( *addr_in );
-	resPtr->ai_addr = (struct sockaddr*)addr_in;
-	resPtr->ai_addr->sa_family = PF_INET;
-	resPtr->ai_next = NULL;
-
-	*res = resPtr;
-
-	return 0;
-}
-
-void freeaddrinfo(struct addrinfo* res )
-{
-    //printf("%s %p %p\n",__FUNCTION__,res,res->ai_addr);
-    free( res->ai_addr );
-    free( res );
 }
