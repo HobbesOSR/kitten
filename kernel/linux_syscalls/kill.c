111d1b64960a65e84acd490b24ba47170cae062d
mjleven <mjleven@gato.sandia.gov>
2010-07-26 10:05:03 -0600

    add sys_tgkill

--- kernel/linux_syscalls/kill.c
+++ kernel/linux_syscalls/kill.c
@@ -2,21 +2,21 @@
 #include <lwk/signal.h>
 #include <lwk/aspace.h>
 
-long
-sys_kill(
-	int	pid,
-	int	signum
+static long do_tgkill( 
+	int tgid, 
+	int tid, 
+	int signum
 )
 {
 	struct siginfo siginfo;
-	id_t aspace_id = (id_t) pid;
+	id_t aspace_id = (id_t) tgid;
 
 	// LWK doesn't support process groups
-	if (pid <= 0)
+	if (tgid <= 0)
 		return -EPERM;
 
 	// Caller wants to verify that pid exists
-	if ((pid > 0) && (signum == 0)) {
+	if ((tgid > 0) && (signum == 0)) {
 		struct aspace *aspace = aspace_acquire(aspace_id);
 		if (!aspace)
 			return -ESRCH;
@@ -35,5 +35,24 @@ sys_kill(
 	siginfo.si_pid   = (int)current->aspace->id;
 	siginfo.si_uid   = (int)current->uid;
 
-	return sigsend(aspace_id, ANY_ID, signum, &siginfo);
+	return sigsend(aspace_id, tid, signum, &siginfo);
+}
+
+long
+sys_kill(
+	int	pid,
+	int	signum
+)
+{
+	return do_tgkill( pid, ANY_ID, signum );
+}
+
+long
+sys_tgkill(
+	int tgid,
+	int tid,
+	int signum
+)
+{
+	return do_tgkill( tgid, tid, signum );
 }
