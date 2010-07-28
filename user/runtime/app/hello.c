9484e1528271e9203ccfa62d2f7895ca9c8f48a9
mjleven <mjleven@gato.sandia.gov>
2010-07-28 08:15:44 -0600

    cleanup

--- user/runtime/app/hello.c
+++ user/runtime/app/hello.c
@@ -3,37 +3,15 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-//#include <pthread.h>
 
 #define __USE_GNU 1
 #include <sched.h>
-
-static void* func(void*ptr)
-{
-#if 0
-    cpu_set_t set;
-    if ( sched_getaffinity( 0, sizeof(set), &set ) ) {
-        printf("sched_getaffinity() failed %s\n",strerror( errno ) );
-    }
-
-    int i;
-    for ( i = 0; i<8; i++ ) {
-        printf("%d\n",CPU_ISSET(i,&set));
-    }
-#endif
-
-    sleep(2);
-    _exit(-45);
-}
 
 int main(int argc, char *argv[], char *envp[] )
 { 
     int i;
     char* env;
     printf("hello exe=`%s` argc=%d\n", argv[0], argc );
-    //return 0;
-    //_exit(0);
-
 
     for ( i = 1; i < argc; i++ ) {
         printf("arg %d \"%s\"\n",i,argv[i]); 
@@ -63,18 +41,6 @@ int main(int argc, char *argv[], char *envp[] )
     fscanf(fp,"%d %d",&nRanks,&myRank);
     printf("nRanks=%d myRank=%d\n",nRanks,myRank);
 
-
-#if 0
-    cpu_set_t set;
-    if ( sched_getaffinity( getpid(), sizeof(set), &set ) ) {
-        printf("sched_getaffinity() failed %s\n",strerror( errno ) );
-    }
-
-    for ( i = 0; i<8; i++ ) {
-        printf("%d\n",CPU_ISSET(i,&set));
-    }
-#endif
-    
     _exit(0);
 
     return 0;
