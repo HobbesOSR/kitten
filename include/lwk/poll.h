6e16a2528f75f344c90cc72b157f764cf0f2331b
mjleven <mjleven@gato.sandia.gov>
2010-08-17 10:36:33 -0600

    moved linux/poll.h content to this file

--- include/lwk/poll.h
+++ include/lwk/poll.h
@@ -1,2 +1,68 @@
-#include <linux/wait.h>
-#include <linux/poll.h>
+#ifndef _LWK_POLL_H
+#define _LWK_POLL_H
+
+#include <lwk/waitq.h>
+#include <lwk/kfs.h>
+
+/* asm/poll.h */
+/* These are specified by iBCS2 */
+#define POLLIN          0x0001
+#define POLLPRI         0x0002
+#define POLLOUT         0x0004
+#define POLLERR         0x0008
+#define POLLHUP         0x0010
+#define POLLNVAL        0x0020
+
+/* The rest seem to be more-or-less nonstandard. Check them! */
+#define POLLRDNORM      0x0040
+#define POLLRDBAND      0x0080
+#ifndef POLLWRNORM
+#define POLLWRNORM      0x0100
+#endif
+
+#define DEFAULT_POLLMASK (POLLIN | POLLOUT | POLLRDNORM | POLLWRNORM)
+
+struct poll_table_struct;
+
+typedef void (*poll_queue_proc)(struct file *, waitq_t *, struct poll_table_struct *);
+
+typedef struct poll_table_struct {
+	poll_queue_proc qproc;
+	unsigned long key;
+} poll_table;
+
+static inline void poll_wait(struct file * filp, waitq_t * wait_address, poll_table *p)
+{
+	if (p && wait_address)
+		p->qproc(filp, wait_address, p);
+}
+
+struct poll_table_entry {
+	struct file *filp;
+	unsigned long key;
+	waitq_entry_t wait;
+	waitq_t *wait_address;
+};
+
+#define N_INLINE_POLL_ENTRIES   ((832-256) / sizeof(struct poll_table_entry))
+static inline void init_poll_funcptr(poll_table *pt, poll_queue_proc qproc)
+{
+	pt->qproc = qproc;
+	pt->key   = ~0UL; /* all events enabled */
+}
+
+
+/*
+ * Structures and helpers for sys_poll/sys_poll
+ */
+struct poll_wqueues {
+	poll_table pt;
+	struct poll_table_page *table;
+	struct task_struct *polling_task;
+	int triggered;
+	int error;
+	int inline_index;
+	struct poll_table_entry inline_entries[N_INLINE_POLL_ENTRIES];
+};
+
+#endif
