b182fe3b1f337dda2bfca350ca48be2353b20869
David DeBonis <ddeboni@sandia.gov>
2012-08-24 13:38:15 -0600

    Infiniband inet address gathered through getifaddrs

--- user/runtime/librdmaPlus/procId.h
+++ user/runtime/librdmaPlus/procId.h
@@ -3,10 +3,14 @@
 #define _rdma_procId_h
 
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <net/if.h>
 #include <types.h>
 
 namespace rdmaPlus {
@@ -16,15 +20,24 @@ static inline ProcId getMyProcID()
     ProcId id;
     id.pid = getpid();
 
-    struct addrinfo *target;
+    struct ifaddrs * ifaddrstruct = NULL;
+    struct ifaddrs * ifa = NULL;
 
-    if ( getaddrinfo( "ib0", NULL, NULL, &target ) != 0 ) {
-            terminal( ,"getaddrinfo()\n");
+    getifaddrs( &ifaddrstruct );
+
+    for ( ifa = ifaddrstruct; ifa != NULL; ifa = ifa->ifa_next) {
+        if ( ifa->ifa_addr->sa_family == AF_INET &&
+             !strncmp( "ib0", ifa->ifa_name, 3) ) {
+            break;
+        }
+    }
+    if ( ifa == NULL ) {
+        terminal( ,"getaddrinfo()\n");
     }
 
-    id.nid = ntohl( ((struct sockaddr_in*)target->ai_addr)->sin_addr.s_addr );
+    id.nid = ntohl( ((struct sockaddr_in *)ifa->ifa_addr)->sin_addr.s_addr );
 
-    freeaddrinfo( target );
+    freeifaddrs( ifaddrstruct );
 
     return id; 
 }
