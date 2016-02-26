22676fa349146b614b7e874ad600724bb368fd7f
Noah Evans <noah.evans@gmail.com>
2016-02-25 23:54:51 -0500

    lstat defaults to stat when not a symlink, no need to leave the call unimplemented. If we add symlinks we'll need to change it, but this should be fine in the interim

--- include/arch-x86_64/unistd.h
+++ include/arch-x86_64/unistd.h
@@ -24,7 +24,7 @@ __SYSCALL(__NR_stat, sys_stat)
 #define __NR_fstat                               5
 __SYSCALL(__NR_fstat, sys_fstat)
 #define __NR_lstat                               6
-__SYSCALL(__NR_lstat, syscall_not_implemented)
+__SYSCALL(__NR_lstat, sys_stat)
 #define __NR_poll                                7
 __SYSCALL(__NR_poll, sys_poll)
 #define __NR_lseek                               8
