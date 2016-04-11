31f655e3f6a2623815f27d65746581091297b83f
Noah Evans <noah.evans@gmail.com>
2016-04-11 14:07:59 -0400

    add .dir-locals.el so that emacs users can edit kitten in a consistent way

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil . ((indent-tabs-mode . t))
+ (c-mode . ((c-file-style . "linux")
+            (indent-tabs-mode . t)
+            (tab-width . 8)))
+ (cc-mode . ((c-file-style . "linux")
+            (indent-tabs-mode . t)
+            (tab-width . 8)))))
