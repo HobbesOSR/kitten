562b9707bf8b0b49facd2cffc5b0bdc5995e29a7
Kevin Pedretti <ktpedre@sandia.gov>
2016-10-24 16:30:49 -0500

    Remove debug printk's

--- kernel/hio/hio_syscalls/hio_fstat.c
+++ kernel/hio/hio_syscalls/hio_fstat.c
@@ -13,14 +13,12 @@ hio_fstat(int     fd,
           uaddr_t buf)
 {
 	int ret;
-	printk("%s: calling hio_fstat on fd %d %p\n", __func__, fd, fdTableFile(current->fdTable, fd) );
 	if ((!syscall_isset(__NR_fstat, current->aspace->hio_syscall_mask)) ||
 	     (fdTableFile(current->fdTable, fd))
 	   ) {
 		return sys_fstat(fd, buf);
 	}
 
-	printk("%s: formatting and execing fstat fd %d buf %p\n", __func__, fd, buf);
 	ret = hio_format_and_exec_syscall(__NR_fstat, 2, fd, buf);
 	return ret;
 }
