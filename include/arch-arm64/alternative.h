6cce2e6202c087f9071a90a3c0c04ba83d215486
Jack Lange <jacklange@cs.pitt.edu>
2021-05-12 23:23:32 -0400

    add missing file

---
+++ include/arch-arm64/alternative.h
@@ -0,0 +1,12 @@
+#ifndef __ARM64_ALTERNATIVE_H__
+#define __ARM64_ALTERNATIVE_H__
+
+/* JRL: 
+ * We are just going to ignore all the workarounds, but leave the macros in  
+ * place for documentation purposes. 
+ */
+
+#define ALTERNATIVE(orig, alt, flag) orig
+
+
+#endif
